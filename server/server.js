05e91ecec3da21b152aca08241437a8e6521d970
Vikram Menon <mvikrammenon@gmail.com>
2023-08-03 16:10:49 -0700

    fix api endpoint typo

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
 
-app.use('/api/v1/template', require('./routes/templateRoutes'))
+app.use('/api/v1/templates', require('./routes/templateRoutes'))
 app.use(express.static('public'))
 
 app.listen(port, () => {
