8637bf15a00d80d17a0f235298f9308afaaec92e
Vikram Menon <mvikrammenon@gmail.com>
2023-08-03 13:52:51 -0700

    setup express, template route, public

---
+++ server/controllers/templateController.js
@@ -0,0 +1,26 @@
+const templatesJson = require('../../data/templates.json')
+const extendedTemplatesJson = require('../../data/extendedTemplate.json')
+
+/**
+ * @description Get all template
+ * @route GET /api/templates
+ * @access public
+ */
+const getTemplates = (req, res) => {
+    res.json(templatesJson)
+}
+
+/**
+ * @description Get all extended template
+ * @route GET /api/templates/extended
+ * @access public
+ */
+const getExtendedTemplates = (req, res) => {
+    res.json(extendedTemplatesJson)
+}
+
+
+module.exports = {
+    getTemplates,
+    getExtendedTemplates
+}
