51482a7e10fa2550e84e8ed0c8c4f5ec11420e40
Vikram Menon <mvikrammenon@gmail.com>
2023-08-03 20:21:42 -0700

    fix image path

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const path = require('path')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
 
 app.use('/api/v1/templates', require('./routes/templateRoutes'))
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(port, () => {
     console.log('Server started on port', port)
